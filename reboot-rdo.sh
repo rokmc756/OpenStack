a852d4ecb6c07fce48ebcb20ec505296b5b3e95a
Jack Moon <rokmc756@gmail.com>
2024-11-24 07:45:56 +0900

    Working Packstack with Multi Nodes

--- reboot-rdo.sh
+++ reboot-rdo.sh
@@ -3,7 +3,7 @@
 for i in $(seq 71 77)
 do
 
-    sshpass -p "changeme" ssh -o StrictHostKeyChecking=no root@192.168.1.$i reboot
+    sshpass -p "changeme" ssh -o StrictHostKeyChecking=no root@192.168.0.$1 reboot
 
 done
 
